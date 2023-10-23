9fed4081acb633234bf4c38406c72302bf65a183
Guilherme Mendes de Oliveira <mecgmo@gmail.com>
2023-10-23 16:01:17 -0300

    First commit

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from typing import Union
+
+from fastapi import FastAPI 
+import debugpy
+debugpy.listen(("0.0.0.0", 5678))
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World!"}
